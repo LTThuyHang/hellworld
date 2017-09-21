faeea04ddddd17f7772ccf0bb791d3de765f5309
nguyentrongvu1607 <nguyentrongvu1607@gmail.com>
2017-09-21 21:21:58 +0700

    abcd

---
+++ Hello World/Hello World/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace Hello_World
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblHelloWorld = new System.Windows.Forms.Label();
+            this.btnShowHelloWorld = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblHelloWorld
+            // 
+            this.lblHelloWorld.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblHelloWorld.AutoSize = true;
+            this.lblHelloWorld.Font = new System.Drawing.Font("Mistral", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHelloWorld.ForeColor = System.Drawing.Color.Purple;
+            this.lblHelloWorld.Location = new System.Drawing.Point(23, 64);
+            this.lblHelloWorld.Name = "lblHelloWorld";
+            this.lblHelloWorld.Size = new System.Drawing.Size(0, 44);
+            this.lblHelloWorld.TabIndex = 0;
+            this.lblHelloWorld.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnShowHelloWorld
+            // 
+            this.btnShowHelloWorld.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnShowHelloWorld.Location = new System.Drawing.Point(73, 187);
+            this.btnShowHelloWorld.Name = "btnShowHelloWorld";
+            this.btnShowHelloWorld.Size = new System.Drawing.Size(124, 41);
+            this.btnShowHelloWorld.TabIndex = 1;
+            this.btnShowHelloWorld.Text = "Show Hello World";
+            this.btnShowHelloWorld.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btnShowHelloWorld);
+            this.Controls.Add(this.lblHelloWorld);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHelloWorld;
+        private System.Windows.Forms.Button btnShowHelloWorld;
+    }
+}
+
